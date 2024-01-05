9a6d30aec87bf0e3450c6bcf19800e1a3d2e2d5a
Team-4585 <84157800+Team-4585@users.noreply.github.com>
2024-01-05 15:51:12 -0700

    Update Limelight.java

--- src/main/java/frc/robot/Limelight.java
+++ src/main/java/frc/robot/Limelight.java
@@ -8,20 +8,30 @@ import edu.wpi.first.networktables.NetworkTableInstance;
 
 public class Limelight extends RoboDevice{
 
-  private NetworkTable table;
+  private NetworkTable networkTable;
   private boolean limelightIsConnected = false;
+
   public Limelight(){
     super("Limelight Sub System");
+    networkTable = NetworkTableInstance.getDefault().getTable("limelight");
   }
 
   public void Initialize(){
-    table = NetworkTableInstance.getDefault().getTable("limelight");
     //Change the modes if needed (look at limelight API for de)
-    table.getEntry("<ledMode>").setNumber(3);
-    table.getEntry("<camMode>").setNumber(0);
-    table.getEntry("<stream>").setNumber(2);
-    table.getEntry("<snapshot>").setNumber(1);
-    table.getEntry("<pipeline>").setNumber(0);
+    NetworkTableInstance.getDefault().getTable("limelight").getEntry("<ledMode>").setNumber(3);
+    NetworkTableInstance.getDefault().getTable("limelight").getEntry("<camMode>").setNumber(1);
+    NetworkTableInstance.getDefault().getTable("limelight").getEntry("<stream>").setNumber(0);
+    NetworkTableInstance.getDefault().getTable("limelight").getEntry("<snapshot>").setNumber(1);
+    NetworkTableInstance.getDefault().getTable("limelight").getEntry("<pipeline>").setNumber(0);
+
+    
+    System.out.println("Initialized!!!!!");
+    networkTable = NetworkTableInstance.getDefault().getTable("limelight");
+    if (networkTable != null){
+      System.out.println("Not null!");
+    } else {
+      System.out.println("Table is null!");
+    }
   }
 
   @Override
@@ -36,29 +46,12 @@ public class Limelight extends RoboDevice{
   }
 
   public boolean isConnected() {
-    System.out.println(limelightIsConnected);
     return limelightIsConnected;
-  }
-
-  public void toggleLED(){
-    table = NetworkTableInstance.getDefault().getTable("limelight");
-
-    System.out.println("Table: " + table);
-    System.out.println("tx: " + table.getEntry("tx"));
-
-    if (table.getEntry("ledMode").getDouble(0) == 0.0){
-      table.getEntry("ledMode").setNumber(1);
-    }
-    else if (table.getEntry("ledMode").getDouble(1) == 1.0){
-      table.getEntry("ledMode").setNumber(0);
-    }
   }
 
   //make sure to set the target
   public boolean isTargetFound() {
-    table = NetworkTableInstance.getDefault().getTable("limelight");
-    
-      NetworkTableEntry tv = table.getEntry("tv");
+      NetworkTableEntry tv = networkTable.getEntry("tv");
       double v = tv.getDouble(0);
       if (v == 0.0f) {
         return false;
@@ -69,50 +62,37 @@ public class Limelight extends RoboDevice{
 
   //tx horizontal offset from crosshair to target (-27 to 27 degrees)
   public double getDegHorizontalFromTarget() {
-    table = NetworkTableInstance.getDefault().getTable("limelight");
-    NetworkTableEntry tx = table.getEntry("tx");
+    NetworkTableEntry tx = networkTable.getEntry("tx");
+
     double x = tx.getDouble(0.0);
     
-    if (tx != null){
-      System.out.println("Degrees Horizontal: " + x);
-    }
-
     return x;
   }
 
   //ty vertical offset from crosshair to target (-20.5 to 20.5 degrees)
   public double getDegVerticalFromTarget() {
-    table = NetworkTableInstance.getDefault().getTable("limelight");
-    NetworkTableEntry ty = table.getEntry("ty");
+    NetworkTableEntry ty = networkTable.getEntry("ty");
     double y = ty.getDouble(0.0);
-    
-    System.out.println("Degrees Vertical: " + y);
-
     return y;
   }
   
   //ta target area (0% to 100% of the of the image)
   public double getTargetArea() {
-    table = NetworkTableInstance.getDefault().getTable("limelight");
-    NetworkTableEntry ta = table.getEntry("ta");
+    NetworkTableEntry ta = networkTable.getEntry("ta");
     double a = ta.getDouble(0.0);
     return a;
   }
 
   //ts shew or rotation (-90 to 90 degrees)
-  public double getSkew() { //Used to be getShewOrRotation. That didn't make sense because the docs say it is skew but the method name was ...ShewOr...
-  table = NetworkTableInstance.getDefault().getTable("limelight");
-    NetworkTableEntry ts = table.getEntry("ts");
+  public double getShewOrRotation() {
+    NetworkTableEntry ts = networkTable.getEntry("ts");
     double s = ts.getDouble(0.0);
-    
-    System.out.println("Degrees Skew: " + s);
-
     return s;
   }
 
   //tl the pipeline's latency contribution (ms); add at least 11 ms for image capture latency
   public double getPipelineLatency() {
-    NetworkTableEntry tl = table.getEntry("tl");
+    NetworkTableEntry tl = networkTable.getEntry("tl");
     double pl = tl.getDouble(0.0);
     return pl;
   }
@@ -126,12 +106,12 @@ public class Limelight extends RoboDevice{
       pipeline = 0;
       throw new IllegalArgumentException("Illegal pipeline: the pipeline can't be greater than 9");
     }
-    table.getEntry("pipeline").setValue(pipeline);
+    networkTable.getEntry("pipeline").setValue(pipeline);
   }
 
   //Returns current pipeline index
   public double getPipelineIndex() {
-    NetworkTableEntry getpipe = table.getEntry("getpipe");
+    NetworkTableEntry getpipe = networkTable.getEntry("getpipe");
     double pipeLine = getpipe.getDouble(0.0);
     return pipeLine;
   }
